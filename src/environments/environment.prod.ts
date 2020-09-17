90a12597beff9e974bcae143a6fba888675f8fb0
Mario Olivares <meolivares06@gmail.com>
2020-09-16 20:57:24 -0400

    [ADD] Testing environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   url: 'https://api.themoviedb.org/3/discover/movie?api_key=4b172fb5c6e8fcb226d23e4fc2e10efd&',
+  poster_size: 'w300',
   apiKey: '4b172fb5c6e8fcb226d23e4fc2e10efd',
-  poster_size: 'w300'
 };
