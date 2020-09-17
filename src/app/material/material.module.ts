e3544e887cdf8b7e672c4f1841d415f9ec34300b
Mario Olivares <meolivares06@gmail.com>
2020-09-16 22:39:40 -0400

    [ADD] Toolbar

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -4,20 +4,28 @@ import { MatSliderModule } from '@angular/material/slider';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
 @NgModule({
   declarations: [],
   imports: [
     MatSliderModule,
     MatCardModule,
     MatButtonModule,
-    MatProgressSpinnerModule
+    MatProgressSpinnerModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonToggleModule
   ],
   exports: [
     MatSliderModule,
     MatCardModule,
     MatButtonModule,
-    MatProgressSpinnerModule
+    MatProgressSpinnerModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonToggleModule
   ],
 })
 export class MaterialModule { }
